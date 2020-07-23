89f63707aba1c8faf1162f3f405b8a43ac7bf55e
山野大樹 <taikiyamano@yamanodaikinoMacBook-ea.local>
2020-07-23 12:17:14 +0900

    postActionCreatorの実装

---
+++ src/actions/postActionCreator.js
@@ -0,0 +1,28 @@
+//ActionCreatorが作成するActionのtypeに渡される文字列を定数に格納しておく→Reducerのswitch処理の条件で使用する為
+export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const LIKE_POST_TOGGLE = 'LIKE_POST_TOGGLE';
+
+
+export const createPost = (name,text,image) => {
+    return {
+        type: CREATE_POST,
+        name,
+        text,
+        image
+    }
+};
+
+export const deletePost = (id) => {
+    return {
+        type: DELETE_POST,
+        id
+    }
+};
+
+export const like_post_toggole = (id) => {
+    return {
+        type: LIKE_POST_TOGGLE,
+        id
+    }
+};
