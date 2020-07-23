5708d295c6079ec663b9592328c5ff0f9eb48889
山野大樹 <taikiyamano@yamanodaikinoMacBook-ea.local>
2020-07-23 21:28:05 +0900

    Postモデルの作成

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+let id = 1;
+
+class Post {
+    constructor(name,text,image) {
+        this._id = id++;
+        this._name = name;
+        this._text = text;
+        this._image = image;
+        this._liked = false
+    };
+    
+    // オブジェクトの値（プロパティ）の値を参照するときはゲッタ（関数）を実行する
+    // オブジェクトの値（プロパティ）の値を変更するときはセッタ（関数）を実装する
+    get id(){
+        return this._id;
+    };
+
+    get name(){
+        return this._name;
+    };
+
+    get text(){
+        return this._text;
+    };
+
+    get image(){
+        return this._image;
+    }
+
+    hasLiked(){
+        return this._liked;
+    }
+
+    likeToggle(){
+        this._liked = !this._liked;
+    }
+}
