a62fc00e4f908e8b09d596f92b65c1e130a6ac8b
山野大樹 <taikiyamano@yamanodaikinoMacBook-ea.local>
2020-07-29 07:31:34 +0900

    Formコンポーネントの作成

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createPost } from '../../actions/postActionCreator'
+import './Form.css';
+
+const Form = (props) => {
+    const [name,setName] = useState('');
+    const [text,setText] = useState('');
+    const [image,setImage] = useState('');
+    // text→実際に値が格納されるもの
+    //setText→（setStateと同じく）、textの値を更新する関数
+    // useState('') →　textのデフォルト値をセット
+
+    const onChangeHandlerForName = (event) => {
+        setName(event.target.value)
+    };
+
+    const onChangeHandlerForText = (event) => {
+        setText(event.target.value);
+    };
+
+    const onChangeHandlerForImage = (event) => {
+        setImage(event.target.value);
+    };
+
+    const onClickButton = () => {
+        if( name && text && image)
+        props.createPost(name,text,image);
+        setName('');
+        setText('');
+        setImage('');
+    }
+
+    
+    return(
+        <div className="whole_wrapper">
+            <h1>React Tweet(With Redux)</h1>
+            <div className="form_space">
+                <div className="form_name">
+                    <label htmlFor="name">投稿者名</label>
+                    <input id="name" name="name" value={name} onChange={onChangeHandlerForName}/>
+                </div>
+                <div className="form_text">
+                    <label htmlFor="text">本文</label>
+                    {/* <textarea id="text" name="text" cols="30" rows="5" onChange={onChangeHandlerForText}></textarea> */}
+                    <textarea id="text" name="text" value={text} cols="30" rows="5" onChange={onChangeHandlerForText}/>
+                </div>
+                <div className="form_image">
+                    <label htmlFor="image">イメージ</label>
+                    <input id="image" name="image" value={image} onChange={onChangeHandlerForImage}/>
+                </div>
+                <button onClick={onClickButton}>投稿</button>
+            </div>
+        </div>
+    )
+    
+}
+
+//mapDispatchTopropsは関数、もしくはオブジェクトをセットする。
+//関数をセットする場合は第一引数にdispatchを受け取る
+//返り値はオブジェクト→今回はオブジェクトの中に関数を定義している
+//このmapDispatchTopropsをconnectの第二引数に渡すことで、props.createPostでpropsからcreatePostを実行できるようになる
+// →createPostを通じてStoreのstateを更新できる（dispatch(action)を行っているから)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createPost: (name,text,image) => {
+            const action = createPost(name,text,image);
+            dispatch(action);
+        }
+    }
+};
+// 短縮形
+// const mapDispatchToProps =　{ createPost };
+
+// コンテナーを作成するためには、connectメソッドを使う
+//connectメソッドの返り値は関数なので、その関数の引数に「props.~」としてstoreのstateやstateを更新するコールバック関数を渡せるようにするコンポーネントをセットする
+export default connect(
+    null,
+    mapDispatchToProps
+)(Form);
